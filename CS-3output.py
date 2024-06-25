3a5e11bf4c2038092e4a0a2b9c64123dccf54d96
2200039129 <2200039129@kluniversity.in>
2024-06-25 19:24:24 +0530

    Add files via upload

---
+++ CS-3output.py
@@ -0,0 +1,12 @@
+C:\Users\kotaa\PycharmProjects\pythonProject13\venv\Scripts\python.exe C:\Users\kotaa\PycharmProjects\pythonProject13\main.py 
+Enter a password to check its complexity: 12345
+Password: 12345
+Length: 5
+Has Uppercase: False
+Has Lowercase: False
+Has Digit: True
+Has Special Character: False
+Strength: Weak
+Feedback: Your password is weak. Password should be at least 8 characters long. Password should include at least one uppercase letter. Password should include at least one lowercase letter. Password should include at least one special character (e.g., !, @, #, $, etc.).
+
+Process finished with exit code 0
